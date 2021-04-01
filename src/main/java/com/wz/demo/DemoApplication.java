941c054be8b89d55e9cb2daf1b1a59286d776a90
ashinwz <john.wang.jw6@roche.com>
2021-04-01 10:18:15 +0800

    add log text

--- src/main/java/com/wz/demo/DemoApplication.java
+++ src/main/java/com/wz/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.wz.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        Environment env =  app.run(args).getEnvironment();
+        LOG.info("running");
+        LOG.info("127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
